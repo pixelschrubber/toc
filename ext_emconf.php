239d964cf9eac512a5ca0ed7064a461d4eab8c12
Ulrich Hecht <ulrich.hecht@hecht-software.de>
2023-02-01 07:44:10 +0100

    #4 TYPO3 11 compatibility (11.5)

--- ext_emconf.php
+++ ext_emconf.php
@@ -23,7 +23,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '11.4.0-11.4.99',
+			'typo3' => '11.4.0-11.5.99',
 			'extbase' => ''		),
 		'conflicts' => array(
 		),
